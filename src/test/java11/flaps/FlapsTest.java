61ec333ab190da74e9e4b797404e16005836de8a
Jan Ouwens <jan.ouwens@gmail.com>
2020-09-12 08:45:40 +0200

    Experiment with multi-release test code

---
+++ src/test/java11/flaps/FlapsTest.java
@@ -0,0 +1,11 @@
+package flaps;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class FlapsTest {
+    @Test
+    public void flaps() {
+        fail("blaaah");
+    }
+}
---
+++ src/test/java15/floeps/FloepsTest.java
@@ -0,0 +1,11 @@
+package floeps;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class FloepsTest {
+    @Test
+    public void failing() {
+        fail("booo");
+    }
+}
